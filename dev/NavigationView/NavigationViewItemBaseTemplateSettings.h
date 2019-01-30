2d1540997042911cf0e2aa5761f1e704b1f2d6f9
Dilip Ojha <dilip.ojha@microsoft.com>
2019-01-29 19:33:13 -0800

    fixed nvibasetemplatesettings

---
+++ dev/NavigationView/NavigationViewItemBaseTemplateSettings.h
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+
+#include "NavigationViewItemBaseTemplateSettings.g.h"
+#include "NavigationViewItemBaseTemplateSettings.properties.h"
+
+class NavigationViewItemTemplateSettings :
+	public winrt::implementation::NavigationViewItemBaseTemplateSettingsT<NavigationViewItemTemplateSettings>,
+	public NavigationViewItemBaseTemplateSettingsProperties
+{
+};
